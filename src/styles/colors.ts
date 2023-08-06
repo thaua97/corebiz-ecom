57d82535822f60f82211b006a1dc070249894224
Thauã Borges <thaua97@gmail.com>
2023-08-06 01:30:04 -0300

    :lipstick: Adiciona estilos globais

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export default {
+  primary: '#F8475F',
+  smoke: '#333333',
+  gray: '#585858',
+  background: '#F2F2F2',
+  black: '#000',
+  neutral: '#fff',
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+import background from './colors';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;900&display=swap');
+    
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    *:focus {
+        outline: 0;
+    }
+
+    html, body, #root {
+        height: 100%;
+    }
+
+    body {
+        -webkit-font-smoothing: antialiased;
+        background-color: ${background} ;
+    }
+
+    body, input, button {
+        font: 14px 'Nunito', sans-serif;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    button {
+        cursor: pointer;
+    }
+`;
